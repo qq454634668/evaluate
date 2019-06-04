98a1e86073c24c8f5bc5a4b2036344757412b7f9
hmini <15357082399@163.com>
2019-06-04 16:08:09 +0800

    c

--- src/main/java/com/evaluate/controller/QuotaController.java
+++ src/main/java/com/evaluate/controller/QuotaController.java
@@ -116,7 +116,7 @@ public class QuotaController {
         Map<String,Object> result = new HashMap<>();
         Map<String,Object> param = new HashMap<>();
         try{
-            param.put("id",id);
+            param.put("parent_id",id);
             List list = quotaService.quotaList(param);
             result.put("data",list);
             result.put("message","指标列表查询成功");
