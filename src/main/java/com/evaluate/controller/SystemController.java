611df7b620bae4b7ca1b722339e4149ada4f3e71
wunan <454634668@qq.com>
2019-06-06 16:26:27 +0800

    新增用户

--- src/main/java/com/evaluate/controller/SystemController.java
+++ src/main/java/com/evaluate/controller/SystemController.java
@@ -1,11 +1,15 @@
 package com.evaluate.controller;
 
 import com.evaluate.service.SystemService;
+import com.evaluate.util.FileUtils;
 import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +24,10 @@ import java.util.Map;
 @Controller
 @CrossOrigin
 public class SystemController {
+
+    @Value("${web.upload-path}")
+    private String path;
+
 
     @Resource
     private SystemService systemService;
@@ -51,20 +59,36 @@ public class SystemController {
             return result;
     }
     /**
+     * 验证用户登录名是否存在
+     */
+    /**
      * 新增用户
      * first_name 姓名
      * login      用户名
      * password   密码
      * role_id    权限ID
      */
-    public Map<String,Object> addUsers(HttpServletRequest request){
+    public Map<String,Object> addUsers(@RequestParam("fileName") MultipartFile file,HttpServletRequest request){
         Map<String,Object> result = new HashMap<>();
         Map<String,Object> param = new HashMap<>();
         try{
-            systemService.addUsers(param);
-            result.put("data",null);
-            result.put("message","用户管理查询成功");
-            result.put("code","200");
+            String login = request.getParameter("login");
+            param.put("login",login);
+            param.put("url","/"+login+".jpg");
+            if(!file.isEmpty()){
+                if (FileUtils.upload(file, path,login)){
+                    systemService.addUsers(param);
+                    result.put("data",null);
+                    result.put("message","用户管理查询成功");
+                    result.put("code","200");
+                }else {
+                    result.put("code", 201);
+                    result.put("message", "上传失败");
+                    result.put("data", null);
+                }
+            }else{
+                //图片为空，传一个空地址
+            }
         }catch (Exception e){
             result.put("message","用户管理查询失败");
             result.put("code","500");
