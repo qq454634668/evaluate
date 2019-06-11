2c5ff14a8c4dba5def967e420fd1f5598f70c864
wunan <454634668@qq.com>
2019-06-11 14:39:51 +0800

    改错

--- src/main/java/com/evaluate/controller/QuotaController.java
+++ src/main/java/com/evaluate/controller/QuotaController.java
@@ -117,7 +117,7 @@ public class QuotaController {
         Map<String,Object> result = new HashMap<>();
         Map<String,Object> param = new HashMap<>();
         try{
-            param.put("parent_id",request.getParameter("id"));
+//            param.put("parent_id",request.getParameter("id"));
             List list = quotaService.quotaList(param);
             result.put("data",list);
             result.put("message","指标列表查询成功");
